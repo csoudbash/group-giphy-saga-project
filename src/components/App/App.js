62ba2cdb69daa52faf3f1d5e308974ad05a19f58
vincesaykao <vtsaykao@hotmail.com>
2022-03-03 13:57:24 -0600

    updated gifform and header jsx, also moved get for api gif

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Header from '../Header/Header.js';
+import gifForm from '../gifForm/gifForm.js'
 
 function App(props) {
   return (
     <div>
+      <Header />
       <h1>Giphy Search!</h1>
     </div>
   );
---
+++ src/components/gifForm/gifForm.js
@@ -0,0 +1,33 @@
+import React, {useState}from 'react';
+// import { useDispatch } from 'react-redux';
+
+
+
+
+
+export default function gifForm() {
+
+    let [gifInput, setGifInput] = useState('');
+
+    // const dispatch = useDispatch();
+
+    
+    const handleInput = (event) => {
+
+    }; // end of handleInput
+
+
+    return (
+        <div id='gifForm'>
+            <input 
+            onClick={handleInput}
+            value={gifInput}
+
+            
+            />
+
+                
+
+        </div>
+    )
+}
