f3553b2e98c1d3167446a1e0ec34e06baa2af8de
Heather Kim <heatherkim0405@gmail.com>
2022-03-03 12:51:05 -0600

    initial commit in the wrong branch

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import {HashRouter as Router, Route, Link} from "react-router-dom";
+
 
 function App(props) {
   return (
-    <div>
-      <h1>Giphy Search!</h1>
-    </div>
+    <Router>
+      <Route path='/' exact>
+        <Header />
+        <gifForm />
+        <gifList />
+      </Route>
+      <Route path='/' exact>
+        <Header />
+        <gifForm />
+        <gifList />
+      </Route>
+
+    </Router>
+ 
   );
 }
 
